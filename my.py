4a6e588c4ce3fc30d701bd5f279caedb3d88de03
abhishek022 <abhishekmicro@hotmail.com>
2017-02-18 16:30:39 +0530

    Update my.py

--- my.py
+++ my.py
@@ -6,10 +6,8 @@ def file_opening():
     print("the file openend is",fob.name)
     print("Is the file closed ",fob.closed)
     print("In which mode file is opened",fob.mode)
-    file_writing(fob)
-    file_reading(fob)
-    fob.close()
-    
+
+
 def file_writing(fo):
     x=input("enter your text")
     fo.write(x)
@@ -19,22 +17,24 @@ def file_reading(fo):
     read_data=fo.readlines()
     for line in read_data:
         words=line.split(",")
-        print(words)
+        print(words)       
 
-        
-        
-from simplecrypt import encrypt, decrypt        
-def lets_crypt():
-    password=input("give your password to crypt")
-    plain_text=input("your text")
-    ecoded_text  = encrypt(password,plain_text)
-    decoded_text = decrypt(password,ecoded_text).decode('utf8')
-    print(decoded_text)
+
+#encrypting and decrypting #
+def encrypt():
+    print(" md5 hashing encryption")
+    import hashlib
+    m = hashlib.md5()
+    m.update(b"Nobody inspects")
+    print("the encrypted text is",m.hexdigest()) 
 
 
 
 file_opening()
-lets_crypt()
+file_writing(fob)
+file_reading(fob)
+fob.close()
+lets_encrypt()
 
 
 
